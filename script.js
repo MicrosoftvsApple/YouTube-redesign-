6dac7080110995cb72cbc6f317fe7d4f9155084f
MicrosoftvsApple <mandicraftyt@gmail.com>
2026-04-09 10:12:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('collapsed');
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
